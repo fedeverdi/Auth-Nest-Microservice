f45043de18d6e9894aa728ba57218b2d0cb1c513
Federico Verdi <federicoverdi@me.com>
2024-11-18 14:07:49 +0100

    Rimosso l'importazione non utilizzata di ConfigService in app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,6 @@ import { LoginDto } from './dto/login.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { MessagePattern, RpcException } from '@nestjs/microservices';
 import { TokenDto } from './dto/token.dto';
-import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
